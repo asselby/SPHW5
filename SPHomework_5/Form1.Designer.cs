81b88dc8fb7dfb3c6832480c6577987cbdaa78cc
БаймуханбетоваА <@A-106-01>
2018-04-27 19:38:01 +0600

    Добавьте файлы проекта.

---
+++ SPHomework_5/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SPHomework_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(437, 98);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputTextBox_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 215);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SPHomework_5/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPHomework_5
+{
+    public partial class Form1 : Form
+    {
+        List<string> list = new List<string>(126000);
+        string hint;
+        public Form1()
+        {
+            InitializeComponent();
+            using (StreamReader reader = new StreamReader("2.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    list.Add(reader.ReadLine());
+                }
+            }
+            list.Sort();
+        }
+
+        private void InputTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+            {
+
+            }
+            else
+            {
+                string[] words = inputTextBox.Text.Split(' ');
+                string lastWord = words[words.Length - 1];
+                try
+                {
+                    int wordPos = list.BinarySearch(lastWord, new MyComparer());
+                    if (wordPos >= 0)
+                    {
+                        hint = list[wordPos];
+                        string hintPart = hint.Substring(hint.Length - (hint.Length - lastWord.Length));
+                        inputTextBox.Text += hintPart;
+                        inputTextBox.Select(inputTextBox.Text.Length - hintPart.Length, hintPart.Length);
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                textBox1.Text = hint;
+            }
+        }
+    }
+}
+public class MyComparer : IComparer<string>
+{
+    public int Compare(string x, string y)
+    {
+        if (x.StartsWith(y))
+        {
+            return 0;
+        }
+        return x.CompareTo(y);
+    }
+}
